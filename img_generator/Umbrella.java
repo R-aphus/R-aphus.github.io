96a39c73a0e86c9b1e12402794e371886a861c52
Ryo Onoda <ryo@MacBookR.local>
2019-02-09 10:43:30 +0900

    umbrella

--- img_generator/Umbrella.java
+++ img_generator/Umbrella.java
@@ -8,6 +8,37 @@ public class Umbrella {
 	public static void main(String[] args) {
 		try {
             int l = 180;
+            double k = 0;
+            BufferedImage img = new BufferedImage(l, l, BufferedImage.TYPE_3BYTE_BGR);
+            Graphics g = img.getGraphics();
+            Color a = new Color(0, 63, 255);
+            Color b = new Color(255, 63, 0);
+//            Color a = new Color(255, 63, 0);
+//            Color b = new Color(0, 106, 255);
+//            Color d = new Color(0, 106, 255);
+//            Color c = new Color(255, 63, 0);
+            for (int i = 0; i < l; i ++) {
+                for (int j = 0; j < l; j ++) {
+/*                    g.setColor(new Color(
+                        (a.getRed() * i * j + b.getRed() * i * (l - j) + c.getRed() * (l - i) * j + d.getRed() * (l - i) * (l - j)) / (l * l),
+                        (a.getGreen() * i * j + b.getGreen() * i * (l - j) + c.getGreen() * (l - i) * j + d.getGreen() * (l - i) * (l - j)) / (l * l),
+                        (a.getBlue() * i * j + b.getBlue() * i * (l - j) + c.getBlue() * (l - i) * j + d.getBlue() * (l - i) * (l - j)) / (l * l)
+                    ));
+*/
+//                    k = Math.sqrt(Math.sqrt((Math.pow(j - l, 2) + Math.pow(i - l / 2.0f, 2)) / (Math.pow(l, 2) + Math.pow(l / 2.0f, 2))));
+                    k = (Math.sqrt((Math.pow(j - l / 2.0f, 2) + Math.pow(i - l / 2.0f, 2)) / (Math.pow(l / 2.0f, 2) + Math.pow(l / 2.0f, 2))));
+                    g.setColor(new Color(
+                        (int)(a.getRed() * k + b.getRed() * (1.0f - k)),
+                        (int)(a.getGreen() * k + b.getGreen() * (1.0f - k)),
+                        (int)(a.getBlue() * k + b.getBlue() * (1.0f - k))
+                    ));
+                    g.drawLine(i, j, i, j);
+                }
+            }
+			g.dispose();
+            ImageIO.write(img, "png", new File("./Umbrella.png"));
+/*
+            int l = 180;
             boolean flag = false;
             float x, y, x2, y2, r2, R2;
             Color Top1 = new Color(243, 115, 53);
@@ -42,9 +73,10 @@ public class Umbrella {
                 }
             }
 			g.dispose();
-			ImageIO.write(img, "png", new File("./Umbrella.png"));
+            ImageIO.write(img, "png", new File("./Umbrella.png"));
+*/
 		}catch(Exception e) {
-			e.printStackTrace();
+            e.printStackTrace();
 		}
-	}
+    }
 }
